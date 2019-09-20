90258feac94c3e51cf7e01b73ebafdd1a1c90821
damithc <damith@gmail.com>
2019-09-20 20:25:08 +0800

    Add initial configs

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@
 ### ./get-reposense.py --tag v1.6.1            # Gets a specific version
 ### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
 
-./get-reposense.py --release
+./get-reposense.py --master
 
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --config ./configs
+java -jar RepoSense.jar --config ./configs --since 06/09/2019 --formats java adoc fxml sh bat gradle txt --timezone UTC+08
